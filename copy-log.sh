68bfecfc3592feb09cf8d72cca4a19abc478d407
impaolo15 <45727306+impapapao@users.noreply.github.com>
2025-05-03 11:05:57 +0800

    add change permission

--- copy-log.sh
+++ copy-log.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 source_dir="$HOME/Documents/logs"
-destination_dir="$HOME/Documents/tmp/logs"
+destination_dir="$HOME/Documents/tmp/ao"
 
 read -p "Enter log name to copy (e.g logname.log): " log_file
 
 source_file="$source_dir/$log_file"
+destination_file="$destination_dir/$log_file"
 
 if [[ ! -d "$destination_dir" ]]; then
         mkdir -p "$destination_dir" || { echo "ERROR: Failed to create destination directory!"; exit 1; }
@@ -20,4 +21,11 @@ if [[ -f "$source_file" ]]; then
 else
         echo "ERROR: File '$log_file' not found in $source_dir"
         exit 1
+fi
+
+if [[ ! -f $destination_file ]]; then
+        echo "Fine not found"
+else
+        chmod 755 "$destination_file"
+        echo "Permission set to 755 on $destination_file"
 fi
