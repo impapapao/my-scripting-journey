fe6fc6867196504663e329c5620e3d58431e882a
impaolo15 <45727306+impapapao@users.noreply.github.com>
2025-05-10 10:10:09 +0800

    initial commit

---
+++ bash-scripts/listing-specific-date.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#user input
+read -p "Enter date (e.g Jan:)" listmonth
+read -p "Enter day (e.g 1:)" listday
+
+#Check if there's a input
+if [[ -z "$listmonth" || -z "$listday" ]]; then
+    echo "User did not input anything."
+    exit 1
+fi                                                                                                                   13                                                                                                                     #listing of specific Month and Day
+ls -ltr | awk  -v month="$listmonth" -v day="$listday" '$6 == month && $7 == day {print $0}'  
